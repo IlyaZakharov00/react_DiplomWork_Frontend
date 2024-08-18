a5e90e5aa19c3a3558143fabcaf03bf857f1f70d
Ilya frontend <zaxarov.ilya.00@inbox.ru>
2024-08-19 00:24:52 +0500

    fix deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import './App.css'
 import './components/Shop/static_files/css/style.css'
-import { HashRouter as Router } from 'react-router-dom'  //BrowserRouter
+import { BrowserRouter as Router } from 'react-router-dom'  //BrowserRouter
 import { Shop } from './components/Shop/Shop/Shop';
 // import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
-    <Router basename='/react_DiplomWork_Frontend' >
+    <Router basename={`/${process.env.PUBLIC_URL}`}>
       <Shop />
     </Router >
   )
